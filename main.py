6a48d138cde2eedb806640cd3e777dde777d16ff
Faustine09 <faustine.richardgauguin@gmail.com>
2025-11-19 22:50:34 +0100

    2

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 
 
-
-
-
-def classFactice(start):                    # entree de dim N
+def classFactice(start, poids, biais):                    # entree de dim N
     def __init__(self):
-        self.start = start
-        self.biais = None
-        self.vectPoids = []
+        self.entree = start
+        self.biais = biais
+        self.vectPoids = poids
+        self.n = len(self.entree)
 
-
-
-    def neuronne(self, entree):             # entree de dim n
-
+    def neurone(self):
         return sortie                       # sortie de dim 1
+
+    def forward(self):
+        sortie = []
+        for i in range(self.n):             # on continue tant que n>1
+            sortie.append(neurone(self))
+        self.entree = sortie
+        self.n -= 1
+
+    def backward(self):                     # sortie du neurone
+        calculgradient()
+        self.vectPoids = newpoids
 
