d614fcecc865eb8ea0673a1cf7282c72d7a20a5f
Faustine09 <faustine.richardgauguin@gmail.com>
2025-11-19 22:37:00 +0100

    1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("Hello World")
-test
+
+
+def classFactice(start):                    # entree de dim N
+    def __init__(self):
+        self.start = start
+        self.biais = None
+        self.vectPoids = []
+
+
+
+    def neuronne(self, entree):             # entree de dim n
+
+        return sortie                       # sortie de dim 1
+
