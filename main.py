b0d41cbeeef3374d38331a42f78cd9fa14efd326
matheostronock <matheo.stronock@gmail.com>
2025-11-20 12:13:27 +0100

    avance apres cours

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
+import numpy as np
 
 
-def classFactice(start, poids, biais):                    # entree de dim N
-    def __init__(self):
+
+x=5
+mat = np.random.randint(0, 256, (x, x))
+
+
+
+class reseauFactice :
+
+    def __init__(self,start, valeurpoids, biais, nb_couche, nb_neuronnesparcouche):
+        self.nb_couche = nb_couche
+        self.nb_neuronnesparcouche = nb_neuronnesparcouche
         self.entree = start
-        self.biais = biais
-        self.vectPoids = poids
-        self.n = len(self.entree)
+        self.biais = np.full((1,nb_couche), 1)
+        self.vectPoids = []
+        for l in range(nb_couche - 1):
+            nbentree = nb_neuronnesparcouche[l]
+            nbsortie = nb_neuronnesparcouche[l + 1]
+            poids = np.full((nbentree, nbsortie), valeurpoids, dtype = float)
+            self.vectPoids.append(poids)
+
 
     def neurone(self):
         return sortie                       # sortie de dim 1
 
+
+
     def forward(self):
-        sortie = []
-        for i in range(self.n):             # on continue tant que n>1
-            sortie.append(neurone(self))
-        self.entree = sortie
-        self.n -= 1
+        a = self.entree
+        for i in range(len(self.vectPoids)):
+            poids = self.vectPoids[i]
+            b = self.biais[i]
+            a = np.dot(a, poids) + b
+        return a
+
+
 
     def backward(self):                     # sortie du neurone
         calculgradient()
